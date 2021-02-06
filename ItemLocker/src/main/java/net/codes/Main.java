c94d5ca860d8ca081198139dd4de7f5a8815cb5f
Codes320 <cody.joseph320@gmail.com>
2021-02-05 18:31:18 -0800

    Cleanup with null pointer

--- ItemLocker/src/main/java/net/codes/Main.java
+++ ItemLocker/src/main/java/net/codes/Main.java
@@ -1,83 +1,46 @@
 package net.codes;
 
 
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 public final class Main extends JavaPlugin {
 
     private HashMap<UUID, List<String>> cache = new HashMap<>();
-    private File playerData;
-    private YamlConfiguration modifyPlayerData;
+    private StorageAPI storage;
+
     
 
     public void onEnable() {
         getCommand("lockitem").setExecutor(new LockCommand(this));
         getServer().getPluginManager().registerEvents(new DropListener(this), (Plugin)this);
+        StorageAPI storage = new StorageAPI(this);
 
         this.saveDefaultConfig();
-        initiateFiles();
+        getStorageAPI().initiateFiles();
 
 
-        if (!this.modifyPlayerData.getConfigurationSection("data").getKeys(false).isEmpty()) {
-            loadItems();
+        if (!getStorageAPI().getModifyPlayerData().getConfigurationSection("data").getKeys(false).isEmpty()) {
+            getStorageAPI().loadItems();
         }
     }
 
     public void onDisable() {
-        if (!cache.isEmpty()) {
-            this.saveItems();
-        }
+        if (!cache.isEmpty()) { getStorageAPI().saveItems(); }
     }
 
     public HashMap<UUID, List<String>> getCache() { return this.cache; }
-    public YamlConfiguration getModifyPlayerData() { return modifyPlayerData; }
-    public File getPlayerData() { return playerData; }
+    public StorageAPI getStorageAPI() { return storage; }
 
-    public void initiateFiles() {
-        playerData = new File(Bukkit.getServer().getPluginManager().getPlugin("ItemLocker").getDataFolder(), "data.yml");
-        if (!playerData.exists()) {
-            try {
-                playerData.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
-        }
-        modifyPlayerData = YamlConfiguration.loadConfiguration(playerData);
-    }
 
-    public void saveItems() {
-        for (Map.Entry<UUID, List<String>> entry : cache.entrySet()) {
-            getModifyPlayerData().set("data." + entry.getKey(), entry.getValue());
-            try {
-                this.getModifyPlayerData().save(this.getPlayerData());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
 
-    }
 
-    public void loadItems() {
-        if (modifyPlayerData.getConfigurationSection("data") != null) {
-        for (String uuid : getModifyPlayerData().getConfigurationSection("data").getKeys(false)) {
-            cache.put(UUID.fromString(uuid), getModifyPlayerData().getStringList("data." + uuid));
-            Bukkit.getConsoleSender().sendMessage(UUID.fromString(uuid).toString());
-        }
 
-        }
-
-    }
 
 }
 
---
+++ ItemLocker/src/main/java/net/codes/StorageAPI.java
@@ -0,0 +1,59 @@
+package net.codes;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class StorageAPI {
+    private File playerData;
+    private YamlConfiguration modifyPlayerData;
+
+    private Main main;
+    public StorageAPI(Main main) { this.main = main; }
+
+
+
+    public void initiateFiles() {
+        playerData = new File(Bukkit.getServer().getPluginManager().getPlugin("ItemLocker").getDataFolder(), "data.yml");
+        if (!playerData.exists()) {
+            try {
+                playerData.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        modifyPlayerData = YamlConfiguration.loadConfiguration(playerData);
+    }
+
+    public void saveItems() {
+        for (Map.Entry<UUID, List<String>> entry : main.getCache().entrySet()) {
+            getModifyPlayerData().set("data." + entry.getKey(), entry.getValue());
+            try {
+                this.getModifyPlayerData().save(this.getPlayerData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+        public void loadItems() {
+            if (modifyPlayerData.getConfigurationSection("data") != null) {
+                for (String uuid : getModifyPlayerData().getConfigurationSection("data").getKeys(false)) {
+                    main.getCache().put(UUID.fromString(uuid), getModifyPlayerData().getStringList("data." + uuid));
+                    Bukkit.getConsoleSender().sendMessage(UUID.fromString(uuid).toString());
+                }
+
+            }
+
+        }
+
+    public YamlConfiguration getModifyPlayerData() { return modifyPlayerData; }
+    public File getPlayerData() { return playerData; }
+
+}
